ecc57cd77078f770250edc4f62722a038fabe4c6
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-03-06 22:51:54 -0500

    added html for rest of the current day and started working on day.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentDay = day.js();
+console.log(currentDay);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
