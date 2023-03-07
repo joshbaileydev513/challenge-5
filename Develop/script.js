cd494509579cd8b7235132188466cd1582b23ab5
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-03-07 16:21:48 -0500

    added data ID's for each hour and got the current day appended to the correct spot on the page

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var currentDay = day.js();
-console.log(currentDay);
+
+// TODO: Add code to display the current date in the header of the page.
+var today = dayjs();
+console.log(today);
+$("#currentDay").text(today.format("dddd, MMMM D YYYY, h:mm:ss a"));
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -22,5 +25,4 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
 });
